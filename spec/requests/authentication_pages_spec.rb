72c4445b94b5d03eb50540544694f13e9c13be98
valariems <valarie@martinstuart.com>
2014-01-12 15:42:51 -0600

    Requiring signed-in users.

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -35,5 +35,25 @@ describe "Authentication" do
         it { should have_link('Sign in') }
       end
     end
+
+    describe "authorization" do
+
+      describe "for non-signed-in users" do
+        let(:user) { FactoryGirl.create(:user) }
+
+        describe "in the Users controller" do
+
+          describe "visiting the edit page" do
+            before { visit edit_user_path(user) }
+            it { should have_title('Sign in') }
+          end
+
+          describe "submitting to the update action" do
+            before { patch user_path(user) }
+            specify { expect(response).to redirect_to(signin_path) }
+          end
+        end
+      end
+    end
   end
 end
