9a5ad869eab2c32a2ce783e1e08649bd4d0bf389
valariems <valarie@martinstuart.com>
2014-01-13 09:56:39 -0600

    Pagination.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:edit, :update]
+  before_action :signed_in_user, only: [:index, :edit, :update]
   before_action :correct_user,   only: [:edit, :update]
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
 
 	def show
 		@user = User.find(params[:id])
@@ -43,7 +47,10 @@ class UsersController < ApplicationController
     # Before filters
 
     def signed_in_user
-      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
     end
 
     def correct_user
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -25,6 +25,7 @@ describe "Authentication" do
       before { sign_in user }
 
       it { should have_title(user.name) }
+      it { should have_link('Users',       href: users_path) }
       it { should have_link('Profile',     href: user_path(user)) }
       it { should have_link('Settings',    href: edit_user_path(user)) }
       it { should have_link('Sign out',    href: signout_path) }
@@ -51,6 +52,27 @@ describe "Authentication" do
           describe "submitting to the update action" do
             before { patch user_path(user) }
             specify { expect(response).to redirect_to(signin_path) }
+          end
+
+          describe "visiting the user index" do
+            before { visit users_path }
+            it { should have_title('Sign in') }
+          end
+        end
+
+        describe "when attempting to visit a protected page" do
+          before do
+            visit edit_user_path(user)
+            fill_in "Email",    with: user.email
+            fill_in "Password", with: user.password
+            click_button "Sign in"
+          end
+
+          describe "after signing in" do
+
+            it "should render the desired protected page" do
+              expect(page).to have_title('Edit user')
+            end
           end
         end
       end
